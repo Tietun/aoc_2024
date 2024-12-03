a4bf661293d59f63d5aa681702fb650eb63ff04d
Ville Rissanen <Ville.Rissanen@agcocorp.com>
2024-12-03 15:15:46 +0200

    3.1

--- aoc_2024/3/main.cpp
+++ aoc_2024/3/main.cpp
@@ -10,8 +10,6 @@ int main(void) {
     std::ifstream input(input_file);
     std::string const file(std::istreambuf_iterator(input), {});
     
-    std::regex const mul_regex(R"(mul\(\d+,\d+\))");
-    
     auto const multiply = [](std::string const& _string)
     {
         std::regex const number_regex(R"(\d+)");
@@ -22,22 +20,18 @@ int main(void) {
         });
     };
     
-    auto const multiplications = std::transform_reduce(std::sregex_iterator(file.cbegin(), file.cend(), mul_regex), std::sregex_iterator(), 0, std::plus(), [&](auto const _mul){
-        return multiply(_mul.str());
-    });
+    auto const add_multiplications = [&](std::string const& _mul_file){
+        static std::regex const mul_regex(R"(mul\(\d+,\d+\))");
+        return std::transform_reduce(std::sregex_iterator(_mul_file.cbegin(), _mul_file.cend(), mul_regex), std::sregex_iterator(), 0, std::plus(), [&](auto const _smatch){
+            return multiply(_smatch.str());
+        });};
     
-    std::cout << "Day 3 Part 1: " << multiplications << std::endl;
+    std::cout << "Day 3 Part 1: " << add_multiplications(file) << std::endl;
     
     std::regex const dont_do_regex(R"(don't\(\)([\S\s]*?)do\(\))");
     auto const enabled_mul_file = std::regex_replace(file, dont_do_regex, "");
     
-    std::cout << enabled_mul_file << std::endl;
-    
-    auto const enabled_multiplications = std::transform_reduce(std::sregex_iterator(enabled_mul_file.cbegin(), enabled_mul_file.cend(), mul_regex), std::sregex_iterator(), 0, std::plus(), [&](auto const _mul) {
-        return multiply(_mul.str());
-    });
-    
-    std::cout << "Day 3 Part 2: " << enabled_multiplications << std::endl;
+    std::cout << "Day 3 Part 2: " << add_multiplications(enabled_mul_file) << std::endl;
     
     return EXIT_SUCCESS;
 }
